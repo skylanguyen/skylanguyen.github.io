58b7ded6072ab2d493fc181a52f30030253105d4
Skyla Nguyen <skng9492@colorado.edu>
2025-09-18 12:34:53 -0600

    just starting out week5

---
+++ wa/wa5java.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+var menu_shown = false;
+
+function showMenu(){
+    var shown = navMenu.classList.toggle("show");
+    
+}
