0a204c6acd531bc8ff7d22954bef94a680a3bcea
Skyla Nguyen <skng9492@colorado.edu>
2025-09-28 22:44:49 -0600

    this class is not my fav rn, week6

--- wa/wa6/wa6java.js
+++ wa/wa6/wa6java.js
@@ -5,22 +5,73 @@ document.addEventListener("DOMContentLoaded", function() { //wait to load html b
     const closeBtn = document.querySelector('.close-btn');
     const thumbnails = document.querySelectorAll('.gallery-thumb');
 
-    thumbnails.forEach(img=> { //when click, change status to activate the lightbox
+    let currentIndex = -1;
+
+    //open lightbox which specific image
+    const openLightbox = (index) =>{
+        const img=thumbnails[index];
+        lightbox.classList.add('active');
+        lightboxImg.src=img.src;
+        lightboxImg.alt=img.alt;
+        currentIndex=index;
+    }
+
+    //close lightbox
+    const closeLightbox=() =>{
+       lightbox.classList.remove('active');
+        lightboxImg.src='';
+        lightboxImg.alt='';
+        currentIndex=-1; 
+    }
+
+    //next and previous images
+    const showNextImage = () => {
+        if (currentIndex < thumbnails.length-1){
+            openLightbox(currentIndex +1);
+        }
+    };
+
+    const showPrevImage = () => {
+        if(currentIndex>0){
+            openLightbox(currentIndex-1);
+        }
+    }
+
+    //event listeners for click
+    thumbnails.forEach((img,index)=> {
         img.addEventListener('click',() => {
-            lightbox.classList.add('active');
-            lightboxImg.src = img.src;
-            lightboxImg.alt = img.alt;
+            openLightbox(index);
         });
     });
 
     closeBtn.addEventListener('click',() => { //deactivate the lightbox when button is clicked
-        lightbox.classList.remove('active');
+        closeLightbox();
     });
 
     lightbox.addEventListener('click',(event) =>{ //if you click on the lightbox, deactivate it
         if(event.target === lightbox){
-            lightbox.classList.remove('active');
+            closeLightbox();
         }
     });
 
+
+//event listeners for keys
+document.addEventListener('keydown',(event) => {
+    if(!lightbox.classList.contains('active')) return;
+
+    switch(event.key){
+        case 'ArrowRight':
+            showNextImage();
+            break;
+        case 'ArrowLeft':
+            showPrevImage();
+            break;
+        case 'Escape':
+            closeLightbox();
+            break;
+    }
+
+
+
+});
 });
