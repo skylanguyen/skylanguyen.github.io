3e5bee94b7513cd791425be08ca9c0f4fbde71c5
Skyla Nguyen <skng9492@colorado.edu>
2025-11-02 16:45:21 -0700

    wa11

---
+++ wa/wa11/wa11.js
@@ -0,0 +1,91 @@
+let favs = [];
+
+async function search() {
+    const query = document.getElementById('search').value;
+    if (!query) return;
+
+    document.getElementById('error').textContent = 'Loading...';
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=b9a5e69d&s=${query}`);
+        const data = await res.json();
+
+        document.getElementById('error').textContent = "";
+
+        if (data.Response === 'True') {
+            showMovies(data.Search);
+        } else {
+            document.getElementById('error').textContent = 'No movies found';
+        }
+    } catch (e) {
+        document.getElementById('error').textContent = 'Error loading movies';
+    }
+}
+
+function showMovies(movies) {
+    let html = '';
+    movies.forEach(m => {
+        html += `
+            <div>
+                <h3> ${m.Title} (${m.Year})</h3>
+                ${m.Poster !== 'N/A' ? `<img src="${m.Poster}" width="100">` : ''}
+                <br>
+                <button onclick='add(${JSON.stringify(m)})'>Add to Favorites</button>
+            </div> `;
+    });
+    document.getElementById('results').innerHTML = html;
+}
+
+function add(movie) {
+    if (!favs.find(f => f.imdbID == movie.imdbID)) {
+        favs.push(movie);
+        showFavs();
+    }
+}
+
+function remove(id){
+    favs = favs.filter(f => f.imdbID !==id);
+    showFavs();
+}
+
+function showFavs() {
+    document.getElementById('count').textContent = favs.length;
+
+    if (favs.length === 0) {
+        document.getElementById('favorites').innerHTML = '<p>No favorites yet</p>';
+        return;
+    }
+
+    let html = '';
+    favs.forEach(m => {
+        html += `
+            <div>
+                <h3> ${m.Title} (${m.Year})</h3>
+                ${m.Poster !== 'N/A' ? `<img src="${m.Poster}" width="100">` : ''}
+                <br>
+                <button onclick="remove('${m.imdbID}')">Remove</button>
+            </div> `;
+    });
+    document.getElementById('favorites').innerHTML = html;
+}
+
+function exportFav(){
+    if(favs.length === 0) return;
+    const blob = new Blob ([JSON.stringify(favs,null,2)], {type: 'applications/json'});
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'favorites.json';
+    a.click();
+}
+
+document.getElementById('search').addEventListener('keydown', e => {
+    if (e.key === 'Enter')
+        search();
+});
+
+function clearFavs(){
+    if(confirm('Delete all favorites?')){
+        favs =[];
+        showFavs();
+    }
+}
